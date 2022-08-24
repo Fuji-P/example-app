0152241b36962ec970c6f4230c8ea2f753c2c454
Fuji-P <nyaguri0417@gmail.com>
2022-08-24 21:59:19 +0900

    アプリケーションのテスト&デプロイ 03/Laravelで構築したアプリケーションをデプロイする

--- tests/Unit/Services/TweetServiceTest.php
+++ tests/Unit/Services/TweetServiceTest.php
@@ -4,6 +4,7 @@ namespace Tests\Unit\Services;
 
 use PHPUnit\Framework\TestCase;
 use App\Services\TweetService;
+use App\Modules\ImageUpload\ImageManagerInterface;
 use Mockery;
 
 class TweetServiceTest extends TestCase
@@ -22,6 +23,9 @@ class TweetServiceTest extends TestCase
             'id' => 1,
             'user_id' => 1
         ]);
+        $imageManager = Mockery::mock(ImageManagerInterface::class);
+        $tweetService = new TweetService($imageManager);
+
         $result = $tweetService->checkOwnTweet(1, 1);
         $this->assertTrue($result);
 
