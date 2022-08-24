7a15704c08f0509fb4a6079be1d2248999f18089
Fuji-P <nyaguri0417@gmail.com>
2022-08-24 22:25:31 +0900

    アプリケーションのテスト&デプロイ 03/Laravelで構築したアプリケーションをデプロイする ※修正

--- app/helpers.php
+++ app/helpers.php
@@ -1,13 +1,12 @@
 <?php
 declare(strict_types=1);
 
-namespace App\Modules\ImageUpload;
-
 if (! function_exists('image_url')) {
 	function image_url(string $path): string
 	{
 		if (app()->environment('production')) {
-			return (string) app()->make(\Cloudinary\Cloudinary::class)->image($path)->secure();
+			return (string) app()->make(\Cloudinary\Cloudinary::class)
+			->image($path)->secure();
 		}
 		return asset('storage/images/' . $path);
 	}
